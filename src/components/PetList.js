400464dd39f1b4a13427626e565939f10fc85d1e
Mehmet Yurtar <mehmetyurtar@gmail.com>
2019-11-24 16:24:07 +0300

    search added

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Pet} from "../components";
 import {getPets} from "../constants";
+import {stringContains} from "../helpers";
 
 
 class PetList extends React.Component{
@@ -26,19 +27,26 @@ class PetList extends React.Component{
 
     componentDidUpdate(prevProps) {
         if(prevProps.activeFilter !== this.props.activeFilter){
-            this.filterPets(this.props.activeFilter);
+            this.filterPets();
+        }
+        if(prevProps.searchValue !== this.props.searchValue){
+            this.filterPets();
         }
     }
 
-    filterPets = (activeFilter) => {
-        if(!activeFilter){
+    filterPets = () => {
+        if(!this.props.activeFilter){
             this.setState({
-                pets: this.state._pets
+                pets: this.state._pets.filter((pet) => {
+                    return stringContains(pet.name, this.props.searchValue)
+                })
             })
         }else{
             this.setState({
                 pets: this.state._pets.filter((pet) => {
-                    return pet.breed === activeFilter;
+                    return pet.breed === this.props.activeFilter;
+                }).filter((filteredPet) => {
+                    return stringContains(filteredPet.name, this.props.searchValue)
                 })
             })
         }
@@ -48,13 +56,13 @@ class PetList extends React.Component{
     render(){
         const Yukleniyor = <div>Yukleniyor</div>;
         const EmptyPets = <div>Bulunamadı</div>;
-        const Pets = <div className="row">
+        const Pets =  [<h3>Gösterilen Pet Sayısı: 5</h3>,<div className="row">
             {
                 this.state.pets.map((pet) => {
                     return <Pet key={Math.random()} {...pet} />
                 })
             }
-        </div>;
+        </div>];
         if(this.state.yukleniyor){
             return Yukleniyor;
         }else if(this.state.pets.length === 0){
