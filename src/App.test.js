f37e511ca3a392dd7c4093c21c7d1c6a278a28e4
KubraSasi <32797543+kubrasasi@users.noreply.github.com>
2020-05-03 01:42:34 +0300

    Petsapp Homework - Kübra Şaşı

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
